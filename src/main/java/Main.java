35120d4ed8cabe30e8f8f68bd983a8a039b7f825
Harsh Panchal <harshhhh1803@gmail.com>
2024-07-03 18:36:25 +0530

    Implemented REPL - Read-Eval-Print Loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,9 @@ public class Main {
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found");
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found");
+        }
     }
 }
