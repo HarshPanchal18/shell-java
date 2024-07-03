e5060a875b87f5505922cbaaf56f55516f21ba67
Harsh Panchal <harshhhh1803@gmail.com>
2024-07-03 20:05:51 +0530

    Removed commands which raised errors while running

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        String[] commands = {"echo", "exit", "type", "cd", "cat"};
+        String[] commands = {"echo", "exit", "type"};
 
         while (true) {
             System.out.print("$ ");
