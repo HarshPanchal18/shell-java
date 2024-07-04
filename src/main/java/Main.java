f412fce6911875537604d4c1983b5fb088884806
Harsh Panchal <harshhhh1803@gmail.com>
2024-07-04 10:19:27 +0530

    Added pwd to commands for type checking

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        String[] commands = {"echo", "exit", "type"};
+        String[] commands = {"echo", "exit", "type", "pwd"};
 
         while (true) {
             System.out.print("$ ");
